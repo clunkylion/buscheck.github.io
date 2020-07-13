2b25613d0122a12764f9eaec43aca4c6a213e2b4
Jose <cote99salamanca@gmail.com>
2020-07-13 15:52:56 -0400

    se configuro el metodo login que hace la peticion al servidor

--- js/class/api.js
+++ js/class/api.js
@@ -1,19 +1,47 @@
 export default class API {
-    login(){
+  login() {
+    const loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const userName = document.getElementById("userName");
+      const password = document.getElementById("password");
+      const alertUser = document.getElementById("alertUser");
+      const alertPassword = document.getElementById("alertPassword");
+      if (userName.value === "") {
+        alertUser.innerHTML = "Debe ingresar su nombre de Usuario";
+        userName.focus();
+        return false;
+      } else if (password.value === "") {
+        alertPassword.innerHTML = "Debe ingresar la contraseña";
+        password.focus();
+        return false;
+      } else {
         let data = {
-            username: "esteban",
-	        password: "tebi123"
-        }
-        fetch('http://157.245.186.141/api/loginWeb/',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(data)
+          username: userName.value,
+          password: password.value,
+        };
+        fetch("http://157.245.186.141/api/loginWeb/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          body: JSON.stringify(data),
         })
-        .then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response));
-    }
+          .then((res) => res.json())
+          .catch((error) => console.error("Error:", error))
+          .then((response) => {
+            if (response.status == 200) {
+              //redireccion a dashboard
+              console.log("Datos Correctos");
+              console.log(response);
+            } else {
+                console.log(response);
+                console.log('Datos Incorrectos o no existen')
+              loginForm.reset();
+            }
+          });
+      }
+    });
+  }
 }
